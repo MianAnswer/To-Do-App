94caf6476fb541745a9e7f3174fa8c625a3b92f6
MianAnswer <miananswer@hotmail.com>
2023-12-02 17:29:06 -0500

    Create XCode Project

---
+++ DoIt/DoIt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DoIt
+//
+//  Created by Mian Answer on 12/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DoIt/DoIt/DoItApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoItApp.swift
+//  DoIt
+//
+//  Created by Mian Answer on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct DoItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
